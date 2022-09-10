139b15219b970997e1a712b85e5811eb95b76094
sostene <ngarukiyimanasostene@gmail.com>
2022-09-10 15:22:14 +0200

    Created about,portfolio,services and contact pages as well as footer using tailwind

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const humberger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const hLink = document.querySelectorAll("#hLink");
+hLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    humberger.classList.toggle("bg-white");
+  });
+});
 humberger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   humberger.classList.toggle("bg-white");
